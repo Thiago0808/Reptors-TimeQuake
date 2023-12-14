7533e748a5dc1a17dce1fffde3a9ba52c3883748
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2023-12-14 00:47:09 -0300

    Audio Funcionando

--- js/audio.js
+++ js/audio.js
@@ -1,11 +1,23 @@
 som = document.querySelectorAll(".som")
+audio = document.getElementById("audio")
+
+estado = "off"
 
 for (let i = 0; i < som.length; i ++){
     som[i].addEventListener("click", function(){
         som[0].classList.remove("animate__animated");
-
         som[0].classList.toggle("none");
         som[1].classList.toggle("none");
+
+        if (estado == "off"){
+            audio.play()
+            estado = "on"
+        }
+        else{
+            audio.pause()
+            estado = "off"
+        }
+
     })
 }
 
