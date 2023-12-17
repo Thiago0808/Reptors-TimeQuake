ea1a67e9b136c39a65fa052742b203bf7e09abf3
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2023-12-17 14:30:34 -0300

    Trocada animação de tremer para wobble

--- js/style.js
+++ js/style.js
@@ -3,13 +3,12 @@ function tremerIcone(icone){
     icone.classList.remove("animate__delay-3s")
     icone.classList.remove("animate__delay-4s")
 
-    
-    icone.classList.add("animate__headShake")
+    icone.classList.add("animate__wobble")
     icone.classList.add("animate__infinite")
     icone.classList.add("infinite")
 
     setTimeout(() => {
-        icone.classList.remove("animate__headShake");
+        icone.classList.remove("animate__wobble");
         icone.classList.remove("animate__infinite");
         icone.classList.remove("infinite");
     }, 1000);
