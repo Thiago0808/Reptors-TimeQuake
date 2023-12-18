8f0219e70a04b6050c0c74dc86a438e445ce9d78
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2023-12-18 16:55:16 -0300

    FIX erro na animação

--- js/audio.js
+++ js/audio.js
@@ -10,6 +10,8 @@ audio.play()
 for (let i = 0; i < som.length; i ++){
     som[i].addEventListener("click", function(){
         som[0].classList.remove("animate__animated");
+        som[1].classList.remove("animate__animated");
+
         som[0].classList.toggle("none");
         som[1].classList.toggle("none");
 
